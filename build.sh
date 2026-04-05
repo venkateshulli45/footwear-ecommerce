64e2bb441af1a6c740e4a16c442bc8efe8964d56
venkateshulli45 <deepcodervenky@gmail.com>
2026-04-05 03:46:54 -0700

    Add Bootstrap icons and build script
    Add Bootstrap Icons assets (CSS + woff/woff2) under static/vendor, introduce build.sh to install requirements, collect static files and run migrations, and add .gitattributes to enforce LF for the build script. Also update templates/base.html and templates/control/base_control.html to reference/use the new static assets.

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Render build: install deps, collect static files into STATIC_ROOT, migrate DB.
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
